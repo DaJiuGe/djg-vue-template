2d07cd634245cd7b75738dd3ac788096f5bdb09f
panlipeng <865589360@qq.com>
2022-04-27 23:17:57 +0800

    chore: add eslint-config dependency

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,11 +4,6 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button']
-    AForm: typeof import('ant-design-vue/es')['Form']
-    AFormItem: typeof import('ant-design-vue/es')['FormItem']
-    AInput: typeof import('ant-design-vue/es')['Input']
-    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     SystemLogo: typeof import('./components/SystemLogo.vue')['default']
   }
